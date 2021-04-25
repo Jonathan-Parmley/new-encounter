4864dfc453610a20c2c06f7dd26a46219f43b9da
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-04-25 11:18:46 +0200

    add new, create, edit, update controller, view page

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,5 +1,6 @@
 class ExperiencesController < ApplicationController
-    
+    before_action :set_experience, only: [:show, :edit, :update, :destroy]
+
     def index
         @experiences = Experience.all
     end
@@ -8,17 +9,38 @@ class ExperiencesController < ApplicationController
     end
 
     def new
+        @experience = Experience.new
     end
 
     def create
+        @experience = Experience.new(experience_params)
+        if @experience.save
+            redirect_to experience_path(@experience_path), notice: 'Experience was successfully created'
+        else
+            render :new
+        end
     end
 
     def edit
     end
 
     def updated
+        @experience.update(experience_params)
+        redirect_to experience_path(@experience_path), notice: 'Experience was successfully updated'
     end
 
     def destroy
+        @experience.destroy
+        redirect_to experiences_path
+    end
+
+    private
+
+    def set_experience
+        @experience = Experience.find(params[:id])
+    end
+
+    def experience_params
+        params.require(:experience).permit(:title, :description, :location, :duration, :price, :user_id)
     end
 end
