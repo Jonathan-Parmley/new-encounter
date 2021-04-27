04f59e6779ba17acf73038c2b6b85d657a203189
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-04-27 12:16:58 +0200

    added Booking model and controllers, updated rputer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :experiences
+  resources :experiences, only: :index
+  resources :users do
+    resources :experiences, exept: :index do
+      resources :bookings
+    end
+  end
+  resources :bookings, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :experiences
 end
