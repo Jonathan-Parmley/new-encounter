6a475ab869eea3370ee3a496a8157aa2caa3ab2b
Tzegai Hidru <thidru@hotmail.de>
2021-04-29 19:56:29 +0200

    Fixed pundit proble on home.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, :raise => false
+  # only: [:home]
+  
+  # def home
+  #   @experiences = policy_scope(Experience).order(created_at: :desc)
+  # end
   
   def home
     @experiences = Experience.all
