27cbbed9f99c77bdff1ef660e52ec935fec7105d
Jonathan Parmley <jonathanparmley@gmail.com>
2021-05-05 21:24:49 +0200

    dashboard cards started

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, :raise => false
   # only: [:home]
-  
+
   # def home
   #   @experiences = policy_scope(Experience).order(created_at: :desc)
   # end
