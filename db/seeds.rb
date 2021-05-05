76e428fa906d483b09bcea4386f0b38cb162a563
Jonathan Parmley <jonathanparmley@gmail.com>
2021-05-05 13:12:22 +0200

    all seed items added, 3 photos each

--- db/seeds.rb
+++ db/seeds.rb
@@ -320,26 +320,296 @@ e9 = [
       ]
 
 e10 = [
-      ""
+      "Cello Meditation Concert",
+
+      "*Great for virtual company events & groups of up to 100! Contact me for
+      customisations (e.g. 30/45-minutes) or to request a time/date not listed.
+
+      Prepare to center your mind and body with this online guided cello
+      meditation hosted by The Wong Janice. We'll start with introductions and a
+      cup of tea together to warm up the heart and the body. Next we'll focus on
+      our breath and start to gain awareness of our bodies to relax, as you take
+      your position on the floor to dive deeper into the meditation with
+      beautiful live cello music.
+
+      Through the combination of music and meditation, you may experience:
+      - healing and deep relaxation
+      - feeling more grounded and emotionally connected
+
+      A little about the music: For each piece played, Janice will first play a
+      low grounding tone and then play sweeping cello melodies on top. Each
+      piece is based on a different emotion - some more uplifting, some more
+      dark. There will be moments of silence in between the pieces to give you a
+      chance to contemplate on the piece and focus again on your breath.
+
+      We will end the concert with a few minutes of sharing in the group.
+
+      To listen to the music ahead of the concert, search for Janice's latest
+      album 'The Chakras: A Deep Cello Journey' on Spotify or iTunes.
+      https://www.thewongjanice.com/listen
+
+      Follow @thewongjanice on Instagram, Facebook and YouTube",
+
+      "https://source.unsplash.com/eenKAWF2dw8",
+      "https://source.unsplash.com/dc2AMLEx4rs",
+      "https://source.unsplash.com/txJmSQY-06o"
 ]
 
-e11
+e11 = [
+        "Family Magic Show and Magic Lesson",
 
-e12
+        "Self-Isolation and Social Distancing are in place everywhere around the
+        world. Many people are worried about how they can spend a fun and
+        meaningful time with their children, family, or friends.
 
-e13
+        Employers around the world also constantly find ways to help their
+        employees to stay connected, healthy and entertained.
 
-e14
+        I created this “Interactive Virtual Magic Show & Lesson” for the above
+        purposes.
 
-e15
+        Preview Video: https://youtu.be/d6WCSEobrEg
 
-e16
+        The human connection between you and me is what makes the magic happen
+        in this experience, with only using everyday objects, without any
+        special boxes or beautiful assistants. :-)
 
-e17
+        Although this is conducted online via ZOOM, it is a highly interactive
+        experience as all the participants will be actively involved in the
+        show. This whole experience is enjoyable for both children and adults
+
+        The show takes around 40-45 minutes, followed by a 15-20 minutes of
+        magic lesson.
+
+        With a little bit of practice, you will have some mind-blowing
+        easy-to-master magic tricks to show your family and friends.
+
+        If you scroll down to the Reviews section, you should be able to imagine
+        better on what this experience is like.
+
+        I support families with young children, please feel free to message me
+        prior to your booking if you plan to attend this experience with your
+        child/children and I will be happy to provide discount.
+
+        This experience is great for special occasions such as :
+        - Virtual Team Building/Corporate Events
+        - Virtual Birthday Parties
+        - Virtual Family Gatherings
+
+        Please feel free to message me for any inquiry and I'll get back to you
+        in a flash!",
+
+        "https://source.unsplash.com/pxM_nRns2Zc",
+        "https://source.unsplash.com/S0_Ep9meLFY",
+        "https://source.unsplash.com/P787-xixGio"
+
+      ]
+
+e12 = [
+        "Hidden Houseboat Concert",
+
+        "Get ready to sing, dance, cheer and laugh your way through these times
+        with music and positive vibes!
+
+        We bring live music straight to your home from our magical houseboat
+        studio in Amsterdam, situated in one of the most sustainable urban
+        developments in all of Europe. During the concert we will also take you
+        on a short tour of the terrain (it's one-of-a-kind!)
+
+        Your experience begins with an introduction and performance of original
+        music by your Amsterdam Music Project Host, followed by a special guest
+        act. You're invited to sing, dance, clap along with us, or simply kick
+        back on your couch and let the sounds and rhythms take you away. Stick
+        around at the end of the performance to hang out with the musicians and
+        like-minded people from all over the world, ask us any questions about
+        our music or beloved city of Amsterdam.
+
+        Are you a musician yourself? Please let us know!
+
+        We also do private shows for special occasions, and can change the music
+          to your liking. Perfect for:
+        -> birthdays
+        -> date nights
+        -> anniversaries
+        -> drinks with friends
+        -> quarantine dance parties
+        -> company/team building and 'outings'
+        Other things to note
+        Please download Zoom on desktop or mobile in advance (no account
+        necessary). Please join the Zoom session 5-10 minutes before the start
+        of the experience to ensure your video and sound are working properly.
+
+        Laptop or TV is recommended so you can see everyone clearly :-)",
+
+        "https://source.unsplash.com/VQgr9d1sUF8",
+        "https://source.unsplash.com/WypHPk6lBFQ",
+        "https://source.unsplash.com/x2lvPNSdw4U"
+      ]
+
+e13 = [
+        "Follow a Plague Doctor Through Prague",
+
+        "Step back into time and virtually experience the empty streets of
+        Prague during the Black Death and plagues of the 17th and 18th
+        centuries. Through video, we'll follow the footsteps of Dr. Alexander
+        Schamsky, an unknown Czech hero who fought the plague in 1713. From the
+        “personal protective equipment” of the time to the deserted landmarks
+        of the city, you’ll immerse yourself in the forgotten history of the
+        pneumonic and bubonic plagues. Our recorded video has no audio as we
+        comment everything live we see. The movie was shot with a professional
+        camera during times of quarantine and shows the abandoned old town.
+        One of our 5 Plague Doctors will take you on the journey to visit the
+        famous Charles Bridge, walk around the Old Town Square, and go into the
+        Jewish Quarter, discovering hidden gems in the dark streets. We´ll stop
+        by the old St. Francis hospital and conclude the tour at the convent of
+        St. Agnes. So, not only will you traverse time, but you’ll also get the
+        sense of travel—all from the comfort of your home.
+        Looking for Prague plague-free? Try our online Nightwatchman Tour:",
+
+        "https://source.unsplash.com/aDxmYZtYj7g",
+        "https://source.unsplash.com/1ak3Z7ZmtQA",
+        "https://source.unsplash.com/9QSK0IChXk0"
+      ]
+
+e14 = [
+        "Cardio Dance with Drag Choreographer",
+
+        "Get ready to cut a (living room) rug with RORYOGRAPHY, a
+        San Francisco-based dance/fitness class in the spirit of
+        a Richard Simmons aerobics tape!
+
+        Throw on your most fabulous movement clothing, clear some space in your
+        living room, and turn your music up...Coach Rory is bringing an
+        only-in-San-Francisco experience to you! No dance experience necessary.
+        All you need is a water bottle and a sense of humor.
+
+        Class consists of a brief introduction, followed by several
+        follow-in-the-moment routines that are designed to make participants
+        laugh and sweat equally for 60 minutes. RORYOGRAPHY brings everyone from
+        children to drag queens together in a joyous celebration of movement.
+        ALL are welcome, and my specialty is people with two left feet.
+
+        This Experience is great for team-building. Book a private experience
+        for your group. Is your group larger than my listed max group size for
+          private groups? Send me a message.
+        Other things to note
+        Let's dance! RORYOGRAPHY is best experienced through a computer or
+        tablet so you can follow Coach Rory's movements.",
+
+        "https://source.unsplash.com/t1uoNDqUs-M",
+        "https://source.unsplash.com/JVnLqWGWVzs",
+        "https://source.unsplash.com/DC5akQJyH4I"
+      ]
+
+e15 = [
+      "Tarot with a therapy dog",
+
+      "GREAT FOR CORPORATE TEAMS, BACHELORETTE PARTIES, BIRTHDAYs, HAPPY HOUR,
+      FRIENDS GET TOGETHER.
+      Contact me for additional availability.
+
+      Experience a fun, new way to connect with friends or co-workers. Come on a
+      trip around the world of tarot, learn how the cards offer guidance and help
+      navigate all types of decisions. This is not the spooky tarot from the
+      movies but consciousness tarot, one that inspires and motivates. No
+      worries, we won't reveal all your secrets to your group members, this is
+      all done in good cheer and taste.
+
+
+      5 star rated team experience by Google, Facebook, Pinterest, Yelp and many
+      others.
+
+      I am a certified tarot consultant, an intuitive and a personal coach
+      backed up by 20+ years of experience and I have training in many
+      leadership modalities.
+
+      We will begin with an icebreaker then learn a brief history of tarot, how
+        the game is constructed and how it has been used throughout the ages.
+      Then, I will guide you in a relaxing guided meditation to get in touch
+      with your intuition.
+      Each participant in the group that wants one get an individual reading.
+
+      PLEASE NOTE THAT IF YOU BOOK AS ONE PERSON, YOU WILL BE JOINED BY OTHERS
+      IN THE ROOM. FOR INDIVIDUAL READINGS, BOOK FOR A PRIVATE GROUP",
+
+      "https://source.unsplash.com/z0snfFRa1fc",
+      "https://source.unsplash.com/8FobV-Ub0eM",
+      "https://source.unsplash.com/iuqxv7kFj64"
+      ]
+
+e16 = [
+        "Dive Under the Sea with a Shark Expert",
+
+        "Dive in and let me share my 20-year career in shark conservation,
+        scientific research, campaigning and education in this story-telling,
+        Shark Smart quiz, creative drawing and fact-filled experience. Plus I’ll
+        give you a copy of my book called 'SHARKS' for free!
+
+        It's fun and suitable for all ages, adults and children alike. It's
+        great for SPECIAL EVENTS, PRIVATE GROUPS and TEAM-BUILDING (I've hosted
+        Microsoft, Apple, Paypal and many more).
+
+        You’ll find out how I overcame my fear of sharks and went from being
+        called Shark Bait to the Shark Warrior, and how an extraordinary shark
+        named Maxine changed my life. You’ll learn about shark research as you
+        follow Maxine’s journey to freedom when I satellite tagged her and set
+        her free after 9 years in captivity.
+
+        My compelling videos and photos will lead you on a virtual underwater
+        adventure into the oceans of the world to meet weird and wonderful
+        sharks, including living dinosaurs! You’ll learn about these amazing
+        animals, what threats they face, and why we need to save them.
+
+        I'll share stories about my global travels to photograph and film
+        different species of sharks, answer questions like why I've never been
+        bitten, and let you in on secrets behind some of my crazy shark
+        awareness campaigns. Most importantly you'll be supporting the
+        conservation work of our nonprofit.
+
+        Please send me a request for a date and time if not listed.",
+
+        "https://source.unsplash.com/GSp6Wio4oFM",
+        "https://source.unsplash.com/GBDkr3k96DE",
+        "https://source.unsplash.com/titrLVi32F8"
+      ]
+
+e17 = [
+      "Booty Shake with Colombian Choreographer",
+
+      "Now, the best thing about dancing at home is that you can actually dance
+      like no one is watching! Don't worry about thinking that you don't have
+      skills... leave it to me!
+
+      Btw, this is a cool gift for girlfriends, so if you want a different date
+      let me know. I have plenty of experience with beginners so all you need to
+      do is take the first step and request a date.
+
+      In my experience in an hour-long class, I can teach you basic or more
+      advanced booty shaking/twerk technics (depending on your preference) that
+      will get you sweating and uplifting your sexiness: queen status.
+
+      Come dance with music from the world, with people from the world!! I'm
+      also an Airbnb Experience host doing dance and nightlife Experiences, so
+      if you want to check out my reviews go to my profile to read what others
+        have said :)
+
+      ******************** If you're looking for personalized
+      dance-lesson-experience I'm opening new slots if you DM***
+
+      Other things to note:
+      You don't need previous experience. Just let me know at the beginning of
+      the class if you have any physical problems I should be aware of, so I can
+      show you variations to the moves.",
+
+      "https://source.unsplash.com/E3ABJKHr1fk",
+      "https://source.unsplash.com/RwjX3Xb6FO8",
+      "https://source.unsplash.com/L9tlXv983dQ"
+      ]
 
 experiences = []
 
-experiences << e0 << e1 << e2 << e3 << e4 << e5 << e6 << e7  << e8 << e9
+experiences << e0 << e1 << e2 << e3 << e4 << e5 << e6 << e7 << e8 << e9 << e10
+experiences << e11 << e12 << e13 << e14 << e15 << e16 << e17
 
 experiences.each do |e|
   file1 = URI.open(e[2])
@@ -354,8 +624,8 @@ experiences.each do |e|
         user_id: firstUser.id
     )
   exp.photos.attach(io: file1, filename: "#{(rand(1000000..1999999))}.png", content_type: 'image/png')
-  exp.photos.attach(io: file2, filename: "#{(rand(1000000..1999))}.png", content_type: 'image/png')
-  exp.photos.attach(io: file3, filename: "#{(rand(1000000..1999))}.png", content_type: 'image/png')
+  exp.photos.attach(io: file2, filename: "#{(rand(1000000..1999999))}.png", content_type: 'image/png')
+  exp.photos.attach(io: file3, filename: "#{(rand(1000000..1999999))}.png", content_type: 'image/png')
   exp.save
   puts "Creating experience: #{e[0]} "
 end
